1817624e02c20a5419d3eed279ab252fc4558df9
Ronakmewara <mewararonak00@gmail.com>
2023-11-09 15:56:42 +0530

    fixed style

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,8 +7,8 @@ function HeroSection() {
   return (
     <div className=' max-w-[100vw] w-full h-[60vh] lg:h-[70vh]' >
       <div className="heading text-center ">
-        <h1 className=' text-4xl  md:text-6xl text-white mb-12 font-bold inter mt-20 md:mt-44 bg-gradient-to-r from-white/10 to-white bg-clip-text text-transparent '>
-        Discover Your new ambition.
+        <h1 className=' text-4xl  md:text-6xl  mb-12 font-bold inter mt-20 md:mt-44 bg-gradient-to-r from-white/10 to-white bg-clip-text text-transparent '>
+        Discover your new ambition.
         </h1>
         <h3 className=' text-xl md:text-2xl text-gray-800 bg-gradient-to-r from-white/10 to-white  inter' > <span className='text-center px-5' > Your Gateway to Endless Stories and Knowledge</span> </h3>
                  <button className='btn-grad inter mx-auto mt-12 '>
